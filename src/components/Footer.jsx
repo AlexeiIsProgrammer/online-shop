72b71a3d608efc92f994053eb72b91aad7118618
kirillbelarus <kgirel@yandex.by>
2023-01-04 19:08:37 +0300

    feat:test commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     return (
         <footer className='footer'>
             <div className='footer__container'>
-
+                
             </div>
         </footer>
     )
