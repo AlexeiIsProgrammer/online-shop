24a175abf003ef5840b76a4b65f457f545b590be
Alexei <avenge.now.r@mail.ru>
2023-01-06 09:20:05 +0300

    feat: init webpack deploy

--- webpack.config.js
+++ webpack.config.js
@@ -99,6 +99,16 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: '[name].[contenthash].css',
         }),
+        new webpack.DefinePlugin({
+            'process.env': {
+                REACT_APP_API_FB_KEY: JSON.stringify(process.env.REACT_APP_API_FB_KEY),
+                REACT_APP_FB_AUTH_DOMAIN: JSON.stringify(process.env.REACT_APP_FB_AUTH_DOMAIN),
+                REACT_APP_FB_DATABASE_URL: JSON.stringify(process.env.REACT_APP_FB_DATABASE_URL),
+                REACT_APP_FB_PROJECT_ID: JSON.stringify(process.env.REACT_APP_FB_PROJECT_ID),
+                REACT_APP_STORAGE_BUCKET: JSON.stringify(process.env.REACT_APP_STORAGE_BUCKET),
+                REACT_APP_FB_MESSAGING_SENDER_ID: JSON.stringify(process.env.REACT_APP_FB_MESSAGING_SENDER_ID),
+            },
+        })
         // new CopyPlugin({
         //     patterns: [
         //         {
